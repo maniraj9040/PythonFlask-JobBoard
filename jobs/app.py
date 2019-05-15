18ace13f4171523e4cfba6b53e8b9f5e079bd2da
maniraj9040 <maniraj9040@gmail.com>
2019-05-15 15:53:02 +0530

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -39,7 +39,7 @@ def job(job_id):
 	jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?',[job_id],single=True)
 	return render_template('job.html',job=job)
 
-app.route('/employer/<employer_id>')
+@app.route('/employer/<employer_id>')
 def employer(employer_id):
 	employer = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employ...yer_id WHERE employer.id = ?:employer_id',singel=True)
 	jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?',[employer_id])
